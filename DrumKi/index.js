2997b0ff33d73b3b09f1d4842f2c4f7f212239f2
badrEladraoui1 <eladraoui.badr@gmail.com>
2024-01-08 12:36:01 +0100

    drunKit

---
+++ DrumKi/index.js
@@ -0,0 +1,20 @@
+function playSound(e) {
+    console.log(e.code)
+    const audio = document.querySelector(`audio[data-key="${e.code}"]`)
+    const drum = document.querySelector(`.drumDiv[data-key="${e.code}"]`)
+
+    if (!audio) return // stop the function
+    audio.currentTime = 0 // rewind to the start for fast repetitions
+    audio.play()
+
+    drum.classList.add('playing')
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return // skips
+    this.classList.remove('playing')
+}
+
+const drums = document.querySelectorAll('.drumDiv')
+drums.forEach(drum => drum.addEventListener('transitionend', removeTransition))
+window.addEventListener('keydown', playSound);
